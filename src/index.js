69a462191d7a084cde0153742f414a5b3ecb0826
evan <stewarea@gmail.com>
2017-04-14 17:49:55 -0700

    add lodash search delay

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-
 const API_KEY = 'AIzaSyA1KzTsbnjZJA0mWmfHZ8QnidST-dBM6Hs';
 
 
@@ -17,8 +17,13 @@ class App extends Component  {
     selectedVideo: null
    };
     // videos is the data.  set array to array of videos = (videos: videos)
+    this.videoSearch('hockey fights');
+  }
 
-    YTSearch({key: API_KEY, term: 'hockeyfights'}, (videos) => {
+
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos:videos,
         selectedVideo: videos[0]
@@ -26,9 +31,13 @@ class App extends Component  {
     });
   }
   render(){
+    //debounce runs the function every 300ms
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
   return (
       <div>
-        <SearchBar />
+       <img className="tube-logo" src="https://lh3.googleusercontent.com/-a2ZqKdy-hFM/VSHzDjhoqjI/AAAAAAAAAAo/D4wqDDQD-vE/es%2Blogo.png"  />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
         onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
